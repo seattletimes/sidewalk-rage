342822dcb244b0e1f63d531c5fee87dabaf9885a
Audrey Carlsen <acarlsen@seattletimes.com>
2015-07-24 17:30:57 -0700

    inset box etc

--- src/js/resultsText.js
+++ src/js/resultsText.js
@@ -1,19 +1,18 @@
 module.exports = function(score){
   return `
     <div class="score">You scored: ${score}</div>
-    <div class="info"><strong>Typical scores range from 5 to 20 with an average of 12.</strong></div>
     <table class="info">
       <tr>
         <td class="icon happy"><i class="fa fa-smile-o"></i></td>
-        <td>If your score is less than 5 you're not an aggressive pedestrian and your pedestrain rage tendency is manageable.</td>
+        <td>If your score is less than 5, you're not an aggressive pedestrian.</td>
       </tr>
       <tr>
         <td class="icon meh"><i class="fa fa-meh-o"></i></td>
-        <td>Scores between 5 and 10 indicate that you have moderate pedestrian rage habits of walking.</td>
+        <td>If your score is between 5 and 10, you have moderate pedestrian rage habits.</td>
       </tr>
       <tr>
         <td class="icon sad"><i class="fa fa-frown-o"></i></td>
-        <td>If your score is greater than 10 your road pedestrian rage tendency is out of control, enough to compromise your ability to remain calm and fair in certain routine, but challenging walking situations.</td>
+        <td>If your score is greater than 10, your pedestrian rage tendency is high. This could compromise your ability to remain calm in some situations.</td>
       </tr>
     </table>
   `
